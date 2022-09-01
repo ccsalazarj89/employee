0538b9f3fd025f0148e0409d93b7c7b15f43e411
Christian Salazar <ccsalazarj>
2022-09-01 16:41:37 -0500

    crossorigin validation is added to avoid restriction

--- src/main/java/com/thales/employee/controllers/EmployeeController.java
+++ src/main/java/com/thales/employee/controllers/EmployeeController.java
@@ -6,14 +6,12 @@ import com.thales.employee.services.EmployeeService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("api/v1/employees")
 public class EmployeeController {
 
